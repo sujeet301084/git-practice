d416105dfa796bc26be3258f408e144e29c40221
sujeetkronik <sujeetkronik@gmail.com>
2020-09-03 22:47:41 +0530

    Test file for practice

---
+++ testmain.py
@@ -0,0 +1,22 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+	def test_do_stuff(self):
+		test_param = 10
+		result = main.do_stuff(test_param)
+		self.assertEqual(result, 15)
+
+	def test_do_stuff2(self):
+		test_param = 'teststring'
+		result = main.do_stuff(test_param)
+		self.assertIsInstance(result, ValueError)
+
+	def test_do_stuff3(self):
+		test_param = None
+		result = main.do_stuff(test_param)
+		self.assertEqual(result, 'please enter number')
+
+
+if __name__ == '__main__':
+	unittest.main()
